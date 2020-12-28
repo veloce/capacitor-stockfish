9ecdac431c80910e8d80a337e0d7780b749a3042
Vincent Velociter <vincent.velociter@gmail.com>
2020-12-28 10:50:27 +0100

    Register removeAllListeners on iOS

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,6 +5,7 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(Stockfish, "Stockfish",
+           CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(getMaxMemory, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(start, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(cmd, CAPPluginReturnPromise);

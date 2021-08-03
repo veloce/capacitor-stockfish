9815e38ba1c0e16f82e2ece808d2c1852e3f1df5
Vincent Velociter <vincent.velociter@gmail.com>
2021-08-03 22:22:08 +0200

    Fix import

--- android/src/main/java/org/lichess/mobileapp/stockfish/Stockfish.java
+++ android/src/main/java/org/lichess/mobileapp/stockfish/Stockfish.java
@@ -14,6 +14,7 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
 
 
 @CapacitorPlugin(name = "Stockfish")
